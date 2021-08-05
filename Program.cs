475b395d8dcdc761be4e8ae2db75e226afcda765
andyterbovich <andy.terbovich@gmail.com>
2021-08-04 20:49:10 -0600

    created dotnet console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_SQLite_creation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
